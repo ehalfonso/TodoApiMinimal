700e9d3184992a28158843968f76dbfe9fdeda98
ehalfonso92@gmail.com <ehalfonso92@gmail.com>
2023-09-05 22:27:25 -0700

    Uso de MapGroup y TypedResults

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -6,15 +6,69 @@ builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList")
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-app.MapGet("/todoitems", async (TodoDb db) =>
-    await db.Todos.ToListAsync());
+var todoitems = app.MapGroup("/todoItems");
 
-app.MapGet("/todoitems/complete", async (TodoDb db) =>
-    await db.Todos.Where(t=>t.IsComplete).ToListAsync());
+todoitems.MapGet("/", GetAllTodos);
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
-    await db.Todos.FindAsync(id));
+async Task<IResult> GetAllTodos(TodoDb db)
+{
+    return TypedResults.Ok(await db.Todos.ToArrayAsync());
+}
 
+todoitems.MapGet("/complete", GetCompleteTodos);
+
+async Task<IResult> GetCompleteTodos(TodoDb db)
+{
+    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
+}
+
+todoitems.MapGet("/{id}", GetTodo);
+
+async Task<IResult> GetTodo(int id,TodoDb db)
+{
+    return await db.Todos.FindAsync(id)
+        is Todo todo
+        ? TypedResults.Ok(todo)
+        : TypedResults.NotFound();
+}
+
+todoitems.MapPost("/", CreateTodo);
+
+async Task<IResult> CreateTodo(Todo todo, TodoDb db)
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+}
+
+todoitems.MapPut("/{id}", UpdateTodo);
+
+async Task<IResult> UpdateTodo(int id,Todo inputTodo,TodoDb db)
+{
+    var todo =await db.Todos.FindAsync(id);
+    if (todo is null) return TypedResults.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+    return TypedResults.NoContent();
+}
+
+todoitems.MapDelete("/{id}", DeleteTodo);
+
+async Task<IResult> DeleteTodo(int id,TodoDb db)
+{
+    if(await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+    return TypedResults.NoContent();
+}
 
 app.Run();
 //https://learn.microsoft.com/es-es/aspnet/core/tutorials/min-web-api?view=aspnetcore-7.0&tabs=visual-studio
+
