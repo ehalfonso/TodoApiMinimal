2b0cdb16ee58b93bcd79270355cdaad7e6291324
ehalfonso92@gmail.com <ehalfonso92@gmail.com>
2023-09-06 18:59:04 -0700

    Terminado el tutorial de API minima

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -6,61 +6,73 @@ builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList")
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-var todoitems = app.MapGroup("/todoItems");
+RouteGroupBuilder todoitems = app.MapGroup("/todoItems");
 
 todoitems.MapGet("/", GetAllTodos);
 
-async Task<IResult> GetAllTodos(TodoDb db)
-{
-    return TypedResults.Ok(await db.Todos.ToArrayAsync());
-}
-
 todoitems.MapGet("/complete", GetCompleteTodos);
-
-async Task<IResult> GetCompleteTodos(TodoDb db)
-{
-    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
-}
 
 todoitems.MapGet("/{id}", GetTodo);
 
-async Task<IResult> GetTodo(int id,TodoDb db)
-{
-    return await db.Todos.FindAsync(id)
-        is Todo todo
-        ? TypedResults.Ok(todo)
-        : TypedResults.NotFound();
-}
-
 todoitems.MapPost("/", CreateTodo);
-
-async Task<IResult> CreateTodo(Todo todo, TodoDb db)
-{
-    db.Todos.Add(todo);
-    await db.SaveChangesAsync();
-
-    return TypedResults.Created($"/todoitems/{todo.Id}", todo);
-}
 
 todoitems.MapPut("/{id}", UpdateTodo);
 
-async Task<IResult> UpdateTodo(int id,Todo inputTodo,TodoDb db)
+todoitems.MapDelete("/{id}", DeleteTodo);
+
+
+
+app.Run();
+//https://learn.microsoft.com/es-es/aspnet/core/tutorials/min-web-api?view=aspnetcore-7.0&tabs=visual-studio
+
+async Task<IResult> GetAllTodos(TodoDb db)
 {
-    var todo =await db.Todos.FindAsync(id);
+    return TypedResults.Ok(await db.Todos.Select(x => new TodoItemDTO(x)).ToArrayAsync());
+}
+
+async Task<IResult> GetCompleteTodos(TodoDb db)
+{
+    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).Select(x=> new TodoItemDTO(x)).ToListAsync());
+}
+
+async Task<IResult> GetTodo(int id, TodoDb db)
+{
+    return await db.Todos.FindAsync(id)
+        is Todo todo
+        ? TypedResults.Ok(new TodoItemDTO(todo))
+        : TypedResults.NotFound();
+}
+
+async Task<IResult> CreateTodo(TodoItemDTO todoItemDTO, TodoDb db)
+{
+    var todoItem = new Todo
+    {
+        IsComplete = todoItemDTO.IsComplete,
+        Name = todoItemDTO.Name
+    };
+    db.Todos.Add(todoItem);
+    await db.SaveChangesAsync();
+
+    todoItemDTO=new TodoItemDTO(todoItem);
+
+    return TypedResults.Created($"/todoitems/{todoItem.Id}", todoItemDTO);
+}
+
+async Task<IResult> UpdateTodo(int id, TodoItemDTO todoItemDTO, TodoDb db)
+{
+    var todo = await db.Todos.FindAsync(id);
     if (todo is null) return TypedResults.NotFound();
 
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+    todo.Name = todoItemDTO.Name;
+    todo.IsComplete = todoItemDTO.IsComplete;
 
     await db.SaveChangesAsync();
     return TypedResults.NoContent();
 }
 
-todoitems.MapDelete("/{id}", DeleteTodo);
-
-async Task<IResult> DeleteTodo(int id,TodoDb db)
+async Task<IResult> DeleteTodo(int id, TodoDb db)
 {
-    if(await db.Todos.FindAsync(id) is Todo todo)
+    if (await db.Todos.FindAsync(id) is Todo todo)
     {
         db.Todos.Remove(todo);
         await db.SaveChangesAsync();
@@ -68,7 +80,3 @@ async Task<IResult> DeleteTodo(int id,TodoDb db)
     }
     return TypedResults.NoContent();
 }
-
-app.Run();
-//https://learn.microsoft.com/es-es/aspnet/core/tutorials/min-web-api?view=aspnetcore-7.0&tabs=visual-studio
-
